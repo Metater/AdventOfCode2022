6aa7ca8ff19e88a7b22fd7680cb93793ed7786de
Connor <61102529+Metater@users.noreply.github.com>
2022-12-07 13:57:57 -0500

    5p1

--- Days/Day5P1.cs
+++ Days/Day5P1.cs
@@ -4,6 +4,34 @@ public class Day5P1 : Day
 {
     public override void Run(List<string> input)
     {
+        GetProcedure(input);
+    }
+
+    private List<List<string>> GetStacks(List<string> input)
+    {
+
+    }
+
+    private List<(int crate, int from, int to)> GetProcedure(List<string> input)
+    {
+        List<(int crate, int from, int to)> procedure = new();
         
+        bool hasProcedureStarted = false;
+        foreach (var line in input)
+        {
+            if (!hasProcedureStarted))
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    hasProcedureStarted = true;
+                }
+
+                continue;
+            }
+
+            Console.WriteLine(line);
+        }
+
+        return procedure;
     }
 }
