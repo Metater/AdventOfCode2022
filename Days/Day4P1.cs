890ccc655bb91c01b9e37390b3f5d068d7ed785d
Connor <61102529+Metater@users.noreply.github.com>
2022-12-07 12:35:41 -0500

    Remove prints

--- Days/Day4P1.cs
+++ Days/Day4P1.cs
@@ -7,7 +7,7 @@ public class Day4P1 : Day
         int count = 0;
         foreach (var line in input)
         {
-            Console.WriteLine(line);
+            //Console.WriteLine(line);
             string[] pair = line.Split(',');
             string[] a = pair[0].Split('-');
             string[] b = pair[1].Split('-');
@@ -15,21 +15,21 @@ public class Day4P1 : Day
             int endA = int.Parse(a[1]);
             int startB = int.Parse(b[0]);
             int endB = int.Parse(b[1]);
-            Console.WriteLine($"sA: {startA}");
-            Console.WriteLine($"eA: {endA}");
-            Console.WriteLine($"sB: {startB}");
-            Console.WriteLine($"eB: {endB}");
+            //Console.WriteLine($"sA: {startA}");
+            //Console.WriteLine($"eA: {endA}");
+            //Console.WriteLine($"sB: {startB}");
+            //Console.WriteLine($"eB: {endB}");
             if (startA >= startB && endA <= endB) // Does A fully contain B?
             {
-                Console.WriteLine("A");
+                //Console.WriteLine("A");
                 count++;
             }
             else if (startB >= startA && endB <= endA) // Does B fully contain A?
             {
-                Console.WriteLine("B");
+                //Console.WriteLine("B");
                 count++;
             }
-            Console.WriteLine();
+            //Console.WriteLine();
         }
         Console.WriteLine(count);
     }
