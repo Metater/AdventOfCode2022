d11c97fbcaa60e99e9db7efc3489154ebfb66d19
Connor <61102529+Metater@users.noreply.github.com>
2022-12-07 15:27:43 -0500

    6p2

--- Days/Day6P2.cs
+++ Days/Day6P2.cs
@@ -4,6 +4,34 @@ public class Day6P2 : Day
 {
     public override void Run(List<string> input)
     {
-        
+        // Received serially
+        // Find start of packet marker
+        // Indicated by 4 unique characters
+        string buffer = input[0];
+        for (int i = 0; i < buffer.Length - 13; i++)
+        {
+            string marker = buffer.Substring(i, 14);
+            //Console.WriteLine(marker);
+            List<char> uniqueChars = new();
+            bool markerValid = true;
+            foreach (var c in marker)
+            {
+                if (!uniqueChars.Contains(c))
+                {
+                    uniqueChars.Add(c);
+                }
+                else
+                {
+                    // marker has non-unique characters
+                    markerValid = false;
+                    break;
+                }
+            }
+            if (markerValid)
+            {
+                Console.WriteLine(i + 14);
+                break;
+            }
+        }
     }
 }
