f7f3760492e9ef0c567f10a0577c5a27c3ed6e90
Connor <61102529+Metater@users.noreply.github.com>
2022-12-07 12:38:18 -0500

    4p2

--- Days/Day4P2.cs
+++ Days/Day4P2.cs
@@ -4,6 +4,33 @@ public class Day4P2 : Day
 {
     public override void Run(List<string> input)
     {
-        
+        int count = 0;
+        foreach (var line in input)
+        {
+            //Console.WriteLine(line);
+            string[] pair = line.Split(',');
+            string[] a = pair[0].Split('-');
+            string[] b = pair[1].Split('-');
+            int startA = int.Parse(a[0]);
+            int endA = int.Parse(a[1]);
+            int startB = int.Parse(b[0]);
+            int endB = int.Parse(b[1]);
+            //Console.WriteLine($"sA: {startA}");
+            //Console.WriteLine($"eA: {endA}");
+            //Console.WriteLine($"sB: {startB}");
+            //Console.WriteLine($"eB: {endB}");
+            if (startA >= startB || endA <= endB) // Does A contain B whatsoever?
+            {
+                //Console.WriteLine("A");
+                count++;
+            }
+            else if (startB >= startA || endB <= endA) // Does B contain A whatsoever?
+            {
+                //Console.WriteLine("B");
+                count++;
+            }
+            //Console.WriteLine();
+        }
+        Console.WriteLine(count);
     }
 }
