5e5241ee97db762cad15efe4f783833880c4193f
Connor <61102529+Metater@users.noreply.github.com>
2022-12-07 14:06:56 -0500

    5p1

--- Days/Day5P1.cs
+++ Days/Day5P1.cs
@@ -4,7 +4,14 @@ public class Day5P1 : Day
 {
     public override void Run(List<string> input)
     {
-        GetProcedure(input);
+        var procedure = GetProcedure(input);
+        foreach (var instruction in procedure)
+        {
+            Console.WriteLine(instruction.crate);
+            Console.WriteLine(instruction.from);
+            Console.WriteLine(instruction.to);
+            Console.WriteLine();
+        }
     }
 
     private List<List<string>> GetStacks(List<string> input)
@@ -29,7 +36,8 @@ public class Day5P1 : Day
                 continue;
             }
 
-            Console.WriteLine(line);
+            string[] instruction = line.Split(' ');
+            procedure.Add((int.Parse(instruction[1]), int.Parse(instruction[3]), int.Parse(instruction[5])));
         }
 
         return procedure;
