b6ae7d3b0167d97bad6aca2b484d6999d100d30f
Connor <61102529+Metater@users.noreply.github.com>
2022-12-09 10:28:33 -0500

    7p1

--- Days/Day7P1.cs
+++ Days/Day7P1.cs
@@ -33,8 +33,8 @@ public class Day7P1 : Day
                             pwd.Clear();
                             break;
                         case "..":
-                            break;
                             pwd.Pop();
+                            break;
                         default:
                             pwd.Push(words[2]);
                             break;
