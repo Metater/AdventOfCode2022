e3f59bd826ae8e8be19e5f6f2fe78231ab73959c
Connor <61102529+Metater@users.noreply.github.com>
2022-12-09 14:12:51 -0500

    8p1

--- Days/Day8P1.cs
+++ Days/Day8P1.cs
@@ -18,11 +18,55 @@ public class Day8P1 : Day
             }
         }
 
+        var trees = GetVisible(0, 0, 0, 1);
+        foreach ((int x, int y) in trees)
+        {
+            Console.WriteLine($"({x}, {y})");
+        }
+
+        bool TryGetTree(int x, int y, out int tree)
+        {
+            if (x >= 0 && x < xLength && y >= 0 && y < yLength)
+            {
+                tree = trees[x, y];
+                return true;
+            }
+            tree = default;
+            return false;
+        }
+
         // Start at a position and iterate by iX and iY, looking for trees taller than the last
         // Returns position of that tree
-        (int x, int y) GetVisible(int x, int y, int iX, int iY)
+        List<(int x, int y)> GetVisible(int x, int y, int iX, int iY)
         {
-            
+            List<(int x, int y)> visibleTrees = new();
+            int lastTree = -1;
+            while (true)
+            {
+                if (TryGetTree(x, y, out int tree))
+                {
+                    if (lastTree == -1) // first cycle
+                    {
+                        lastTree = tree;
+                    }
+                    else // not first cycle
+                    {
+                        if (tree > lastTree) // visible from outside of grid
+                        {
+                            lastTree = tree;
+                            visibleTrees.Add(tree);
+                        }
+                        if (tree == 9)
+                        {
+                            return visibleTrees;
+                        }
+                    }
+                }
+                else
+                {
+                    return visibleTrees;
+                }
+            }
         }
     }
 }
