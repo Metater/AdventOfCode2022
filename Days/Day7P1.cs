5bccb70fcbd293662aa6b6c98fedef7efd7de278
Connor <61102529+Metater@users.noreply.github.com>
2022-12-09 10:31:33 -0500

    7p1

--- Days/Day7P1.cs
+++ Days/Day7P1.cs
@@ -14,7 +14,7 @@ public class Day7P1 : Day
         Stack<string> pwd = new();
         string GetCurrentPath()
         {
-            string path = $"/{string.Join('/', pwd.Reverse())}";
+            string path = $"/{string.Join('/', pwd)}";
             return path;
         }
         Dictionary<string, List<(int size, string name)>> directories = new();
