b4444a710dbda948dbcae9d1eb4e3099e2b463cc
Connor <61102529+Metater@users.noreply.github.com>
2022-12-09 14:31:29 -0500

    8p1

--- Days/Day8P1.cs
+++ Days/Day8P1.cs
@@ -18,11 +18,81 @@ public class Day8P1 : Day
             }
         }
 
+        /*
         var test = GetVisible(0, 0, 0, 1);
         foreach ((int x, int y) in test)
         {
             Console.WriteLine($"({x}, {y})");
         }
+        */
+
+        List<(int x, int y)> visibleTrees = new();
+        bool IsAlreadyVisible(int x, int y)
+        {
+            foreach ((int eX, int eY) in visibleTrees)
+            {
+                if (eX == x && eY == y)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        // Check left -> right
+        for (int y = 1; y < yLength - 1; y++)
+        {
+            var sweep = GetVisible(0, y, 1, 0);
+            foreach ((int x, int y) in sweep)
+            {
+                if (!IsAlreadyVisible(x, y))
+                {
+                    visibleTrees.Add((x, y));
+                }
+            }
+        }
+
+        // Check right -> left
+        for (int y = 1; y < yLength - 1; y++)
+        {
+            var sweep = GetVisible(xLength - 1, y, -1, 0);
+            foreach ((int x, int y) in sweep)
+            {
+                if (!IsAlreadyVisible(x, y))
+                {
+                    visibleTrees.Add((x, y));
+                }
+            }
+        }
+
+        // Check top -> bottom
+        for (int x = 1; x < xLength - 1; x++)
+        {
+            var sweep = GetVisible(x, 0, 0, 1);
+            foreach ((int x, int y) in sweep)
+            {
+                if (!IsAlreadyVisible(x, y))
+                {
+                    visibleTrees.Add((x, y));
+                }
+            }
+        }
+
+        // Check bottom -> top
+        for (int x = 1; x < xLength - 1; x++)
+        {
+            var sweep = GetVisible(x, yLength - 1, 0, -1);
+            foreach ((int x, int y) in sweep)
+            {
+                if (!IsAlreadyVisible(x, y))
+                {
+                    visibleTrees.Add((x, y));
+                }
+            }
+        }
+
+        Console.WriteLine(visibleTrees.Length);
+        // Plus perimeter
 
         bool TryGetTree(int x, int y, out int tree)
         {
@@ -53,8 +123,8 @@ public class Day8P1 : Day
                     {
                         if (tree > lastTree) // visible from outside of grid
                         {
-                            lastTree = tree;
                             visibleTrees.Add((x, y));
+                            lastTree = tree;
                         }
                         if (tree == 9)
                         {
